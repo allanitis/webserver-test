fa4f132dfd11c7ece9423737f7310cc8c70bc648
Matt Allan <matthewa021@gmail.com>
2019-06-26 12:10:05 +0100

    Add files via upload
    these are raw javascript files

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("hey javascript from the web server is loading ");
+});
+console.log("hey this is from js");
+
+alert("oh hey here we are!");
